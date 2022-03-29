5d42049ccdf0e7a50ccd3872e43eb7ece453b8da
henryferrari <hcvferrari@gmail.com>
2022-03-29 14:56:31 +0100

    test drove fizzBuzz function

---
+++ fizzbuzz.test.js
@@ -0,0 +1,16 @@
+const fizzBuzz = require('./lib/fizzbuzz');
+
+describe ('fizzBuzz', () => {
+  test('it returns Fizz if given 3', () => {
+    expect(fizzBuzz(3)).toBe('Fizz');
+  });
+  test('it returns Buzz if given 5', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+  });
+  test('it returns FizzBuzz if given 15', () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+  });
+  test('it returns 8 if given 8', () => {
+    expect(fizzBuzz(8)).toBe(8);
+  });
+});
